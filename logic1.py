960777989dd9fdd9f584d6d2ee3f15d380ab539c
Chelsey McKinney <chelseymckinney@gmail.com>
2019-01-18 22:10:32 -0600

    completing logic1 problems

--- logic1.py
+++ logic1.py
@@ -80,3 +80,108 @@ def cigar_party(cigars, is_weekend):
 			return 2
 		else:
 			return False 
+
+
+	#problem 5 
+	#sorta_sum--Given 2 ints, a and b, return their sum. However, sums in range 10..19 inclusive, are forbidden, so in that case just return 20. def sorta_sum(3,4)-->7
+
+	def sorta_sum(a,b):
+		sum = a + b 
+	if a >= 10 and b<= 19:
+		return sum
+	elif a <=10 or b <= 10:
+		return sum
+	else:
+		return False 
+
+	def sorta_sum(a,b):
+		sum = a + b
+	if sum in range(10,19):
+		return 20
+	elif a <= 10 or b <= 10:
+		return sum
+	elif a >= 10 and b <= 19:
+		return sum
+	else:
+		return False 
+
+	#problem 6
+	#alarm_clock--Given a day of the week encoded as: 0 = Sun  1 = Mon 2 = Tue...6 = Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm shoud be "7:00" and of the weekend it should be "10:00". Unless we are on vacation--then on weekdays it should be "10:00" and weekends it should be "off". 
+	
+	#0 = Sun
+	#1 = Mon ("7:00")
+	#2 = Tues ("7:00")
+	#3 = Wed  ("7:00")
+	#4 = Thurs ("7:00")
+	#5 = Fri ("7:00")
+	#6 = Sat -- "10:00"
+
+	def alarm_clock(day, vacation):
+		if day == vacation:
+			return "10:00"
+		elif day >= 1 and day <= 6:
+			return "7:00"
+		else:
+			return "off"
+
+	def alarm_clock(day, vacation):
+		if day == vacation:
+			return "10:00"
+		elif day > 1 and day < 6:
+			return "7:00"
+		else:
+			return "off"
+		
+		#problem 7
+		#love6--The number 6 is a truly great number: Given 2 int values, a and b, return True if either on is 6. Or, if their sum or difference is 6. Note: the function abs(num) computes the absolute value of a number
+
+		def love6(a,b):
+			if a and b == 6:
+				return True
+			elif a == b or b == 6:
+				return True
+			else:
+				return False 
+
+		def love6(a,b):
+			if a + b or a - b == 6:
+				return True
+			elif a == 6  or b == 6:
+				return True
+			else:
+				return True 
+
+	
+	#problem 8
+	#in1to10--Given a number n, return True if n is in the range 1...10 inclusive. Unless outside_mode is True, in which case return True if the number is less or equal to 1, or greater or equal to 10.
+
+	def in1to10(n, outside_mode):
+		n --> range 1..10 --> True
+		outside_mode --> True --> n <= 1 or n >= 10
+
+	def in1to10(n,outside_mode):
+		if outside_mode:
+			return True
+		elif n in range(1,10):
+			return True
+		else:
+			return False 
+
+
+	#problem 9
+	#near_ten--given a non-negative number "num", return True if num is within 2 or a multiple of 10. Note:(a % b) is the remainder of dividing a by b, so (7%5) is 2. 
+	#near_ten(12)->True   near_ten(17)--False
+
+	def near_ten(num):
+		if num % 2 == 10:
+			return True
+		elif num % 2 == 0:
+			return True
+		else:
+			return False
+
+	def near_ten(num):
+		if num % 2 == 10 or num %2 >= 10:
+			return True
+		else:
+			return False 
